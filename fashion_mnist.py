de59975f720228d83e53de8580044bc4e118316f
bob <vrakosky.083@gmail.com>
2018-11-06 01:58:15 +0100

    Setting network parameters

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -7,3 +7,10 @@ from keras.optimizers import Adam, Adadelta, RMSprop
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Lambda, SpatialDropout2D
+
+#------------- Setting network parameters
+epochs = 1
+num_classes = 10
+batch_size = 300
+input_shape = (28, 28, 1)
+filepath = "cnn_model_best.hdf5"
