d8536e3e34f2c1d3bf109a75bd128aabdf4e86ef
bob <vrakosky.083@gmail.com>
2018-11-06 03:02:40 +0100

    Checkpoint implementation, saving on a single .hdf5 : Saving and Reloid

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -32,14 +32,32 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 #------------- Model
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+print(filepath)
+modelExist = os.path.exists(filepath)
+if(modelExist == True):
+	model = load_model(filepath)
+else:
+
+	model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=input_shape, padding='same'))
+	model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+	model.add(MaxPooling2D(pool_size=(3, 3)))
+	model.add(Dropout(0.25))
+
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(lr=0.001),metrics=['accuracy'])
+model.summary()
+
+	# Display evaluate
+score=model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+	# checkpointer 
+checkpointer=ModelCheckpoint(filepath=filepath, verbose=1, monitor="val_acc", save_best_only=True)
 
 model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(lr=0.001),metrics=['accuracy'])
 model.summary()
@@ -51,5 +69,13 @@ model.fit(x_train, y_train,
 			verbose=1,
 			shuffle=True,
 			validation_data=(x_test, y_test),
-			callbacks=[tensorboard, checkpointer, reduce_lr])
+			callbacks=[checkpointer])
 score=model.evaluate(x_test, y_test, verbose=0)
+
+# Reinitinilize learning rate
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(lr=0.001),metrics=['accuracy'])
+model = load_model(filepath)
+
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
