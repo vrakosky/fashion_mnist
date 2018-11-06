216967a55930c30810a79da2afa4ce14d92f1ff0
bob <vrakosky.083@gmail.com>
2018-11-06 02:01:05 +0100

    Import the Fashion_MNIST dataset

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -14,3 +14,17 @@ num_classes = 10
 batch_size = 300
 input_shape = (28, 28, 1)
 filepath = "cnn_model_best.hdf5"
+
+#------------- Dataset of 60,000 28x28 training images & 10,000 28x28 test images.
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+#------------- Convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
